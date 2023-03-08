3694ad361e6769ea5e5f375d7163179207a73c9d
Inigo San Gil <inigo.sangil@gmail.com>
2023-03-08 13:25:28 -0700

    cleaning the code

--- sp_Ref_Patient_Drugs_Administered.sql
+++ sp_Ref_Patient_Drugs_Administered.sql
@@ -8,51 +8,49 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
-
-
-
-
-
-
-
-
-
-
 CREATE PROCEDURE [dbo].[sp_Ref_Patient_Drugs_Administered]
 
 -- Drugs administered in-house for each patient
 -- Flag activities as whether they are used in scheduling or billing and classify by validity and use 
 -- Originally built for RS21 OMOP extract, but useful in other visit reporting.
--- Must be maintained with addition of new scheduling activities.  Billing CPTs will be picked up automatically from IDX
---
+-- Must be maintained with addition of new scheduling activities.  
+-- Billing CPTs will be picked up automatically from IDX
+
 
 AS
 BEGIN
--- [dbo].[vw_PharmOrd_Derived_Data] to this table
--- Table/Field References changed 
 
+SET NOCOUNT ON;
 
--- for RS21 OMOP model, we need to select "procedures" from Charge Table to be able to use CPT codes to classify the appointments. But, we need to exclude DRUG CPT codes from the "Procedure" extract.
--- to do -- reach out to Andi/Candy about what it means if a drug has adm_amount = 0 -- give example to them to help determine if I can exclude these from the extract
--- drop table #last, #OLD
+if object_id('tempdb..#OLD') is not null
+	drop table #Pat_Visits;
+
+if object_id('tempdb..#adm_NEW') is not null
+	drop table #adm_NEW;
+
+if object_id('tempdb..#DrugCodeMap') is not null
+	drop table #DrugCodeMap;
+
+if object_id('tempdb..#NEW') is not null
+	drop table #NEW;
+
+if object_id('tempdb..#Data') is not null
+	drop table #Data;
+
 SELECT * 
 INTO #OLD
 FROM Ref_Patient_Drugs_Administered
-
-
 
 DECLARE @LastAdmDtTm dateTime
 SET @LastAdmDtTm = 
 (Select isNULL(max(adm_Start_DtTm) , '2010-01-01')
 FROM Ref_Patient_Drugs_Administered)
-SELECT @LastAdmDtTm 
 
-select * from mosaiq.dbo.drug
---  drop table #adm_NEW
+
 SELECT 
 	RXA.RXA_SET_ID, 
-	RXA.RXO_Set_ID,  -- if Pharm Order data is needed, use RXO_Set_ID to join  Mosaiq.dbo.vw_PharmOrd_Derived_Data to this table
+	RXA.RXO_Set_ID,  -- if Pharm Order data is needed, use RXO_Set_ID to 
+                         -- join  Mosaiq.dbo.vw_PharmOrd_Derived_Data to this table
 	RXA.Orc_set_ID,  -- Order data  
 	RXA.Pat_ID1, 
 	mosaiq.dbo.fn_Date(RXA.adm_dtTm) as Adm_Date,
@@ -60,19 +58,14 @@ SELECT
 	RXA.Adm_End_DtTm,
 	RXA.Adm_code,		-- Drug.Drg_id
 
-	CASE -- get a CPT code in case drug cannot be identified via label, type or MEDID (which is used to map to RxNorm and CXV vocabularies)
+	CASE -- get a CPT code in case drug cannot be identified via label, type or MEDID 
+             --  (which is used to map to RxNorm and CXV vocabularies)
 		when RXA.Adm_code <> 0 -- drug_id assigned (this is the norm) 
 		then Drg.Drug_Label		
-		Else isNull(cptRef.CPT_desc_mq, ' ')		--no drug name listed, only PRS_ID (CPT) given.  Occurred Pre-2016 for saline and dextrose solutions administered
+		Else isNull(cptRef.CPT_desc_mq, ' ')  --no drug name listed, only PRS_ID (CPT) given. 
+                                                   -- Occurred Pre-2016 for saline and dextrose solutions administered
 	END Drug_Label,				-- add a new column for this in extract 
 	isNULL(Drg.generic_name, '') as Drug_Generic_Name,
-	/*
-	CASE -- get a CPT code in case drug cannot be identified via label, type or MEDID (which is used to map to RxNorm and CXV vocabularies)
-		when RXA.Adm_code <> 0 -- drug_id assigned (this is the norm) 
-		then mosaiq.dbo.fn_GetObsDefLabel(Drg.Drug_Type)
-		else isnull(cptRef.CPT_desc_mq, ' ')  -- no drug name listed, only PRS_ID (CPT) given.  Occurred Pre-2016 for saline and dextrose solutions administered
-	END Drug_Generic_Name,  -- add a new column for this in extract 
-	*/
 	isNULL(mosaiq.dbo.fn_GetObsDefLabel(Drg.Drug_Type), ' ') as Drug_Type, -- add a new column for this in extract
 	drg.MEDID AS FDB_MedID,  -- add a new column for this in extract
 	drg.RMID as FDB_RMID,
@@ -97,7 +90,6 @@ SELECT
 	RXA.PRS_ID			-- populated pre-2016 when no drug_id specified (ex: Saline solns) -- Unique key for Mosaiq.dbo.CPT and MosaiqAdmin.dbo.Ref_CPTs_and_Activities 
 into #adm_NEW
 	FROM MOSAIQ.dbo.PharmAdm RXA 
---	INNER JOIN MosaiqAdmin.dbo.RS21_Patient_List_for_Security_Review Subset on RXA.pat_id1 = Subset.Pat_ID1
 	LEFT JOIN mosaiq.dbo.Drug drg on RXA.Adm_code = Drg.DRG_ID 
 	LEFT JOIN MosaiqAdmin.dbo.Ref_CPTs_and_Activities cptRef on RXA.PRS_ID = cptRef.PRS_ID --and cptRef.cpt_code is not null and cptRef.cpt_code <> ' ' -- RXA.prs_id is null except when there is no RXA.Adm_code specified)
 	LEFT JOIN Mosaiq.dbo.Orders orc	on rxa.orc_set_id = orc.orc_set_id and orc.version = 0
@@ -106,30 +98,22 @@ into #adm_NEW
 	and RXA.adm_code <> 929 -- drug_label = 'DO NOT USE'  -- there is also adm_code 383 (Acetaminophen DO NOT USE) ??
 	and RXA.adm_dtTm >= '2010-01-01'
 	and RXA.adm_DtTm >  @LastAdmDtTm
--- WHAT ABOUT Adm_AMT = 0??? why does this happen? -- keeping 0 in for Betsy's Drug Reconciliation
-	--and RXA.pat_id1 =12480  and mosaiq.dbo.fn_date(adm_dttm) = '2015-07-15'  -- had dups
 
 
-
---select count(*)
---from #adm_NEW
-
-select * from Mosaiq.dbo.DrugCodeMapping 
-
--- drop table #DrugCodeMap
 -- get single mapping by medId and codeset (remove duplicates)
-select medId, rmid, codeset, CodeValue, CodeDescription, BestMatch, CodeType,
+SELECT medId, rmid, codeset, CodeValue, CodeDescription, BestMatch, CodeType,
 Row_Number () OVER (Partition by medId, codeset order by  bestMatch desc, codevalue desc) as seq  
 	-- if there is 1-best-match select that one
-	-- if there are multiple best-matches, select the record with the highest codevalue (arbitrary assuming higher # is newer)
-	-- if there are no best batches, select the record with the highest best-match (arbitrary assuming higher # is newer )
-into #DrugCodeMap -- get only 1 map per medId in case of multiple entries in DrugCodeMapping 
-from Mosaiq.dbo.DrugCodeMapping 
-where (medId is not null or RMID is not null) --and codeset is not null
-order by codeDescription,  seq --codeset, codevalue, codedescription
+	-- if there are multiple best-matches, select the record with the highest codevalue 
+        -- (arbitrary assuming higher # is newer)
+	-- if there are no best batches, select the record with the highest best-match 
+        -- (arbitrary assuming higher # is newer )
+INTO #DrugCodeMap -- get only 1 map per medId in case of multiple entries in DrugCodeMapping 
+FROM Mosaiq.dbo.DrugCodeMapping 
+WHERE (medId is not null or RMID is not null) --and codeset is not null
+ORDER BY codeDescription,  seq            --codeset, codevalue, codedescription
 
--- drop table #NEW
-select Distinct 
+SELECT DISTINCT
 	#adm_NEW.PAT_ID1,
 	#adm_NEW.Adm_Date,
 	#adm_NEW.Adm_Start_DtTm,
@@ -156,22 +140,11 @@ select Distinct
 	#adm_NEW.RXO_Set_ID, 
 	#adm_NEW.ORC_Set_ID,
 	getDate() as run_date
-into #NEW
-from #adm_NEW
-left join MosaiqAdmin.dbo.Ref_SchSets Sch on #adm_NEW.pat_id1 = sch.pat_id1 and #adm_NEW.Adm_Date = sch.appt_date  
-left join #DrugCodeMap dcm1 on #adm_NEW.FDB_MedID = dcm1.MedID and dcm1.CodeSet = 1  and dcm1.seq = 1 -- CodeSet = RxNorm   --seq eliminates dups
-left join #DrugCodeMap dcm2 on #adm_NEW.FDB_MedID = dcm2.MedID and dcm2.CodeSet = 2  and dcm2.seq = 1 -- CodeSet = CVX		--seq eliminates dups
-
-
-
-
-
-
--- select count(*) from #adm_NEW
--- select count(*) from #NEW
--- select count(*) from #OLD
--- drop table #data
-
+INTO #NEW
+FROM #adm_NEW
+LEFT JOIN MosaiqAdmin.dbo.Ref_SchSets Sch on #adm_NEW.pat_id1 = sch.pat_id1 and #adm_NEW.Adm_Date = sch.appt_date  
+LEFT JOIN #DrugCodeMap dcm1 on #adm_NEW.FDB_MedID = dcm1.MedID and dcm1.CodeSet = 1  and dcm1.seq = 1 -- CodeSet = RxNorm   --seq eliminates dups
+LEFT JOIN #DrugCodeMap dcm2 on #adm_NEW.FDB_MedID = dcm2.MedID and dcm2.CodeSet = 2  and dcm2.seq = 1 -- CodeSet = CVX		--seq eliminates dups
 
 /* In 5% of the records, drugs were administered but there is no captured appt.  
 Example:  pat_id1=5xxx7 / appt_dt=2021-11-02 --> patient was scheduled for Rad Tx but was in hospital so appt was statused as B(reak) -- but patient was given morhpine?  was patient brought here?  
@@ -183,7 +156,7 @@ select count(*) from #adm2
 select count(*) from #adm2 where first_SchSet_of_day is null
 select max(adm_date) from #adm2 where first_SchSet_of_day is null
 */
--- drop table #Data
+
 SELECT DISTINCT *
 INTO #Data
 FROM (
@@ -245,20 +218,11 @@ FROM (
 		GetDate() as RunDate
 	FROM #NEW
 ) as A
--- drop table #Data
-
-
---select count(*) from #old
---select count(*) from #new
---select count(*) from #data
---select distinct run_Date from #data
 
 TRUNCATE TABLE MosaiqAdmin.dbo.Ref_Patient_Drugs_Administered
 INSERT INTO MosaiqAdmin.dbo.Ref_Patient_Drugs_Administered
 Select * 
-from #data
-
-
+from #Data
 
 END
 GO
